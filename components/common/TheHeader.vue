3cb6bb426237616df60f874694dd01772f495f19
konapeso <n2kgt1@gmail.com>
2019-12-13 01:31:35 +0900

    create new app

---
+++ components/common/TheHeader.vue
@@ -0,0 +1,55 @@
+<template>
+    <header id="header">
+        <nav id="main_navigation"><nuxt-link to="/"><img src="@assets/images/konamiota.jpg"></nav>
+        <nav id="sns_navigation">
+            <ul>
+                <li><span><a href="https://www.facebook.com/konami.ota.73">
+                <img src="@/assets/icons/FB_icon.png" alt="" height="15px" width="15px">
+                </a></span></li>
+                <li><span><a href="https://www.instagram.com/kona_pasyo/">
+                <img src="@/assets/icons/INSTAGRAM.png" alt="" height="15px" width="15px">
+                </a></span></li>
+                <li><span><a href="https://www.instagram.com/kona_pasyo/">
+                <img src="@/assets/icons/TWITTER.png" alt="" height="15px" width="15px">
+                </a></span></li>
+            </ul>
+        </nav>
+    </header>
+</template> 
+
+<style>
+    #header{
+   z-index: 20;
+    position: fixed;
+    top: 0px;
+    width: 100%;
+    height: 90px;
+    background-color:#fff;
+
+    }
+    #main_navigation {
+    font-weight: bold;
+    margin-left: auto;
+    margin-right: auto;
+    width: 630px;
+    text-align: center;
+}
+ 
+    #sns_navigation {
+    position: fixed;
+    right: 60px;
+    top: 40px;
+    width: 202px;
+ }
+    #nav ul{
+        list-style: none;
+    }
+
+    #nav li {
+    height: 10px;
+    display: inline-block;
+}
+
+    
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <div id="content">
+  
+  </div>
+  </v-app>      
+</template>
+
+<style>
+  #content{
+    width:100%;
+        height:100vh;
+        background:url('~assets/images/white-clouds.jpg');
+        background-size: 100%;
+  }
+</style>
